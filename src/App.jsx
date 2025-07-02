c175cc7ee120db04b1306f039629d93baa44f80f
fredo omar rabemanantsoa <rabmanomar@gmail.com>
2025-07-02 23:15:21 +0300

    premier commite

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+        <nav>
+          <img src=''>
+          </img>
+          <ul> 
+            <li>ACCUEIL</li>
+            <li>ACTUALITES</li>
+            <li>BOURSE D'ETUDES</li>
+            <li>INSCRIPTIOIN</li>
+          </ul>
+            <button className='bg-white'>INTRANET </button>
+        </nav>
+      <section>
+        <h1 >Haute Ecoles d'Informatique </h1>
+        <p>"L'education est l'armes le plus puissant pour changer le monde"
+          selon NELSON MANDELA. " L'EDUCATION DANS L'informatique est une arme tres puissante pour lutter contre la povrette a Madagascar "
+          selon HEI. C'est notre mission.
+        </p>
+        <p>Une infrastructure de qualiter et un programme pedagosique qualiter
+          repond au besoin du marcher pour ll'emploiyabilite de nos etudiants.
+        </p>
+        <p>
+          Des entreprise partenaire et une equipes pedagogique et administrative qui travaille sans relache pour la montee en competences de nos etudiants.
+        </p>
+        <button>Inscrivez-vous ici </button>
+        <button> Emploi du temps</button>
+        <button>Programme pedagogique</button>
+
+        <p>Habilitation MESupRES,suivant l'arret nn°31309/2023</p>
+      </section>
+      <section>
+        <h2> A propos de nous </h2>
+        <i> +3ans </i>
+        <p>Depuis 3 ans, nous formons ceux qui décodent le futur, aux métiers de l’informatique. Nous sommes un groupe d’experts, de professionnels et des passionnés de l’informatique de
+        Madagascar et du monde entier.</p>
+        <i>+250 etudiants</i>
+        <p>Plus de 250 étudiants et plus d’une dizaine d’entreprises ont déjà fait confiance à notre formation habilitée par l’État malgache. Avec eux tous, nous sommes fiers de pouvoir parler 
+        de la “grande famille HEI”.</p>
+        <i>Notre mission</i>
+        <p>Notre mission est d’amener des jeunes malgaches sur des domaines porteurs comme l’intelligence artificielle, la cybersécurité ou la programmation afin de favoriser leur employabilité et 
+        le développement de Madagascar.</p>
+      </section>
+
+      
+      
+      
+    </>
+  )
+}
+
+export default App
